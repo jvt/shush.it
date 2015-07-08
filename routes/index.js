9edf484f62dc48294bc8ab6a58f0132f53256740
Joe Torraca <joe@joetorraca.com>
2015-07-08 18:45:50 -0400

    Created statis routes for "Help" and "Privacy & Terms" pages

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Express' });
 });
 
+/* GET help page. */
+router.get('/help', function(req, res, next) {
+  res.render('help', { title: 'Express' });
+});
+
+/* GET privacy page. */
+router.get('/privacy', function(req, res, next) {
+  res.render('privacy', { title: 'Express' });
+});
+
 module.exports = router;
