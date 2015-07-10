6beb045471b10d8761a4b965800baf3498142e26
Henri Watson <henri@henriwatson.com>
2015-07-10 02:09:19 -0400

    add models for filter exports, filters themselves, and users

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var exphbs  = require('express-handlebars');
+var bookshelf = require('bookshelf');
 var nconf = require('nconf');
 var OAuth = require('oauth');
 var path = require('path');
 var session = require('express-session');
 
-var routes = require('./routes/index');
-var routes_auth = require('./routes/auth');
+var knexfile = require('./knexfile');
 
 var app = express();
 
@@ -38,6 +38,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 GLOBAL.config = nconf.argv().env().file({ file: path.join(__dirname, 'config.json') });
 
+var knex = require('knex')(knexfile);
+
+bookshelf.DB = require('bookshelf')(knex);
+
 GLOBAL.oauth = new OAuth.OAuth(
   'https://api.twitter.com/oauth/request_token',
   'https://api.twitter.com/oauth/access_token',
@@ -53,6 +57,9 @@ app.use(session({
   resave: false,
   saveUninitialized: false
 }));
+
+var routes = require('./routes/index');
+var routes_auth = require('./routes/auth');
 
 app.use('/', routes);
 app.use('/auth', routes_auth);
---
+++ models/user.js
@@ -0,0 +1,22 @@
+var bookshelf = require('bookshelf').DB;
+
+var ExportedFilter = require("./exportedfilter").model;
+var Filter = require("./filter").model;
+
+exports.model = bookshelf.Model.extend({
+  tableName: 'users',
+  hasTimestamps: true,
+
+  exportedFilters: function() {
+    return this.hasMany(ExportedFilter, 'user');
+  },
+
+  filters: function() {
+    return this.hasMany(Filter, 'owner');
+  }
+
+});
+
+exports.collection = bookshelf.Collection.extend({
+  model: exports.model
+});
