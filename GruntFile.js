3823b80ebea46861a3038a0fae918801390e0b9f
Joe Torraca <joe@joetorraca.com>
2015-07-18 22:29:26 -0400

    Added Twitter icons after our names in the footer

--- GruntFile.js
+++ GruntFile.js
@@ -41,7 +41,8 @@ module.exports = function(grunt) {
         },
         files: {
           'public/stylesheets/style.css': '<%= project.assets%>/src/scss/style.scss',
-          'public/stylesheets/materialize.css': '<%= project.assets%>/src/scss/materialize.scss'
+          'public/stylesheets/materialize.css': '<%= project.assets%>/src/scss/materialize.scss',
+          'public/stylesheets/fontawesome.css': 'public/components/font-awesome/scss/font-awesome.scss'
         }
       },
       dist: {
@@ -51,7 +52,8 @@ module.exports = function(grunt) {
         },
         files: {
           'public/stylesheets/style.css': '<%= project.assets%>/src/scss/style.scss',
-          'public/stylesheets/materialize.css': 'public/components/materialize/sass/materialize.scss'
+          'public/stylesheets/materialize.css': '<%= project.assets%>/src/scss/materialize.scss',
+          'public/stylesheets/fontawesome.css': 'public/components/font-awesome/scss/font-awesome.scss'
         }
       }
     },
