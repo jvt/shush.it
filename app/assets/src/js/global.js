f21b89f19a20c1f5da2877972bffc5320ccc37f3
Joe Torraca <joe@joetorraca.com>
2015-07-19 00:45:58 -0400

    Navbar now includes a hamburger icon in mobile which includes search box. Closes #22

--- app/assets/src/js/global.js
+++ app/assets/src/js/global.js
@@ -1,5 +1,5 @@
-$(document).ready(function()
-{
+$(document).ready(function() {
+  $(".button-collapse").sideNav();
   $('.install-button').on('click', function(event)
   {
     var button = $(this);
