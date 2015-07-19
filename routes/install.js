48c8fd6a2da3ff73d58a0fa6e7676a2bf31248c0
Joe Torraca <joe@joetorraca.com>
2015-07-18 23:57:37 -0400

    Stores a new row in the `filter_export_log` table on every /install/ page hit unless user/filter combo is already stored. Closes #20

--- routes/install.js
+++ routes/install.js
@@ -2,10 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 var Filter   = require('../models/filter').model;
+var exFilter = require('../models/exportedfilter.js').model;
 
 /* GET install page. */
 router.get('/:id/', function(req, res, next) {
   var filterID = req.params.id;
+
+  if (req.session.userID && filterID) {
+    new exFilter({
+      user: req.session.userID,
+      filter: filterID
+    }).fetch().then(function(storedTransaction) {
+      if (storedTransaction == undefined) {
+        new exFilter({
+          user: req.session.userID,
+          filter: filterID
+        }).save();
+      }
+    });
+  }
+
   new Filter({'id': filterID})
     .fetch()
     .then(function(model)
