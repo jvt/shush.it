ad42e569171abef8a06f0e4c6c93c08543947147
Joe Torraca <joe@joetorraca.com>
2015-07-19 00:45:18 -0400

    Updated spacing and Grunt now watches JS files in src

--- GruntFile.js
+++ GruntFile.js
@@ -9,89 +9,93 @@ var config = nconf.argv().env().file({ file: path.join(__dirname, 'config.json')
  */
  module.exports = function(grunt) {
   /**
-   * Configuration
+ * Configuration
+ */
+ grunt.initConfig({
+  /**
+   * Get package meta data
    */
-   grunt.initConfig({
-    /**
-     * Get package meta data
-     */
-     pkg: grunt.file.readJSON('package.json'),
-    /**
-     * Set project object
-     */
-     project: {
-      app: 'app',
-      assets: '<%= project.app %>/assets',
-      src: '<%= project.assets %>/src',
-      css: [
-      '<%= project.src %>/scss/style.scss'
-      ],
-      js: [
-      '<%= project.src %>/js/*.js'
-      ]
-    },
-    /**
-     * Sass
-     */
-     sass: {
-      dev: {
-        options: {
-          style: 'expanded',
-          compass: true
-        },
-        files: {
-          'public/stylesheets/style.css': '<%= project.assets%>/src/scss/style.scss',
-          'public/stylesheets/materialize.css': '<%= project.assets%>/src/scss/materialize.scss',
-          'public/stylesheets/fontawesome.css': 'public/components/font-awesome/scss/font-awesome.scss'
-        }
+   pkg: grunt.file.readJSON('package.json'),
+  /**
+   * Set project object
+   */
+   project: {
+    app: 'app',
+    assets: '<%= project.app %>/assets',
+    src: '<%= project.assets %>/src',
+    css: [
+    '<%= project.src %>/scss/style.scss'
+    ],
+    js: [
+    '<%= project.src %>/js/*.js'
+    ]
+  },
+  /**
+   * Sass
+   */
+   sass: {
+    dev: {
+      options: {
+        style: 'expanded',
+        compass: true
       },
-      dist: {
-        options: {
-          style: 'compressed',
-          compass: true
-        },
-        files: {
-          'public/stylesheets/style.css': '<%= project.assets%>/src/scss/style.scss',
-          'public/stylesheets/materialize.css': '<%= project.assets%>/src/scss/materialize.scss',
-          'public/stylesheets/fontawesome.css': 'public/components/font-awesome/scss/font-awesome.scss'
-        }
-      }
-    },
-    /**
-     * Knexmigrate
-     */
-     knexmigrate: {
-      config: {
-        directory: './migrations',
-        tableName: 'knex_migrations',
-        database: {
-          client: 'mysql',
-          connection: config.get('database')
-        }
-      }
-    },
-    /**
-     * Minify Javascript files
-     */
-     minified : {
       files: {
-        src: [
-          '<%= project.assets%>/src/js/global.js',
-        ],
-        dest: 'public/javascripts/'
-      },
-      options : {
-        sourcemap: false,
-        allinone: false
+        'public/stylesheets/style.css': '<%= project.assets%>/src/scss/style.scss',
+        'public/stylesheets/materialize.css': '<%= project.assets%>/src/scss/materialize.scss',
+        'public/stylesheets/fontawesome.css': 'public/components/font-awesome/scss/font-awesome.scss'
       }
     },
-    /**
-     * Watch
-     */
-     watch: {
-      sass: {
+    dist: {
+      options: {
+        style: 'compressed',
+        compass: true
+      },
+      files: {
+        'public/stylesheets/style.css': '<%= project.assets%>/src/scss/style.scss',
+        'public/stylesheets/materialize.css': '<%= project.assets%>/src/scss/materialize.scss',
+        'public/stylesheets/fontawesome.css': 'public/components/font-awesome/scss/font-awesome.scss'
+      }
+    }
+  },
+  /**
+   * Knexmigrate
+   */
+   knexmigrate: {
+    config: {
+      directory: './migrations',
+      tableName: 'knex_migrations',
+      database: {
+        client: 'mysql',
+        connection: config.get('database')
+      }
+    }
+  },
+  /**
+   * Minify Javascript files
+   */
+   minified : {
+    files: {
+      src: [
+        '<%= project.assets%>/src/js/global.js',
+      ],
+      dest: 'public/javascripts/'
+    },
+    options : {
+      sourcemap: false,
+      allinone: false
+    }
+  },
+  /**
+   * Watch
+   */
+  watch: {
+    sass: {
       files: '<%= project.src %>/scss/{,*/}*.{scss,sass}',
       tasks: ['sass:dev']
+    },
+    javascript: {
+      files: '<%= project.src %>/js/*.js',
+      tasks: ['minified']
     }
   }
 });
