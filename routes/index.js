d38f0a893a332aa56e74f4ee0d41f59dd887e12c
Joe Torraca <joe@joetorraca.com>
2015-07-20 18:20:05 -0400

    Added logic for viewing your account filters

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var filter   = require('../models/filter').model;
 var exFilter = require('../models/exportedFilter').collection;
+var user     = require('../models/user').model;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -29,6 +31,44 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Home', numResults: model.length, results: model.toJSON(), rowLayout: rows });
   });
 });
+
+/* GET user's submitted filters */
+router.get('/:twitterScreenName', function(req, res, next) {
+  if (!req.session.userID) {    
+    var err = new Error();
+    err.status = 404;
+    err.message = 'Looks like you\'ve either navigated to a page that doesn\'t exist or you followed a broken link to get here';   
+    next(err);
+  } else {
+    var twitterScreenName = req.params.twitterScreenName;
+
+    new user({ twitter_screenname: twitterScreenName })
+    .fetch()
+    .then(function(userModel) {
+      if (!userModel) {
+        var err = new Error();
+        err.status = 404;
+        err.message = 'Looks like you\'ve either navigated to a page that doesn\'t exist or you followed a broken link to get here';   
+        next(err);
+      }
+
+      if (userModel.attributes.id === req.session.userID) {
+        new filter({ owner: userModel.attributes.id })
+        .fetchAll()
+        .then(function(filtersModel) {
+          res.render('myfilters', { title: 'My filters', filters: filtersModel.toJSON() });
+        });
+      } else {
+        var err = new Error();
+        err.status = 404;
+        err.message = 'Looks like you\'ve either navigated to a page that doesn\'t exist or you followed a broken link to get here';   
+        next(err);
+      }
+
+    });
+  }
+});
+
 
 /* GET help page. */
 router.get('/help', function(req, res, next) {
