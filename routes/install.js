5591c8db3f47962c91f0d90b78885d539f9a6f20
Joe Torraca <joe@joetorraca.com>
2015-07-20 18:44:47 -0400

    Rewrote install to validate if a filter id is actually a filter

--- routes/install.js
+++ routes/install.js
@@ -8,36 +8,61 @@ var exFilter = require('../models/exportedfilter.js').model;
 router.get('/:id/', function(req, res, next) {
   var filterID = req.params.id;
 
-  if (req.session.userID && filterID) {
-    new exFilter({
-      user: req.session.userID,
-      filter: filterID
-    }).fetch().then(function(storedTransaction) {
-      if (storedTransaction == undefined) {
+  new Filter({ id: filterID })
+  .fetch()
+  .then(function(filterModel) {
+    if (filterModel) {
+      if (req.session.userID) {
         new exFilter({
           user: req.session.userID,
           filter: filterID
-        }).save()
-        .catch(function() {
-          req.flash('error', 'There was an error attempting to install that filter');
-          res.redirect('back');   
+        }).fetch()
+        .then(function(storedTransaction) {
+          if (storedTransaction == undefined || storedTransaction == null) {
+            new exFilter({
+              user: req.session.userID,
+              filter: filterID
+            }).save()
+            .then(function()
+            {
+              var filterPattern = filterModel.attributes.pattern;
+              var filterType    = filterModel.attributes.type;
+              if (filterType === 'regex') {
+                res.redirect('tweetbot:///mute/keyword?muteLists=1&regex=1&text=' + filterPattern);
+              } else {
+                res.redirect('tweetbot:///mute/keyword?muteLists=1&regex=0&text=' + filterPattern);
+              }
+            })
+            .catch(function() {
+              req.flash('error', 'There was an error attempting to install that filter');
+              res.redirect('back');   
+            });
+          } else {
+            var filterPattern = filterModel.attributes.pattern;
+            var filterType    = filterModel.attributes.type;
+            if (filterType === 'regex') {
+              res.redirect('tweetbot:///mute/keyword?muteLists=1&regex=1&text=' + filterPattern);
+            } else {
+              res.redirect('tweetbot:///mute/keyword?muteLists=1&regex=0&text=' + filterPattern);
+            }
+          }
         });
-      }
-    });
-  }
-
-  new Filter({'id': filterID})
-    .fetch()
-    .then(function(model)
-    {
-      var filterPattern = model.attributes.pattern;
-      var filterType    = model.attributes.type;
-      if (filterType === 'regex') {
-        res.redirect('tweetbot:///mute/keyword?muteLists=1&regex=1&text=' + filterPattern);
       } else {
-        res.redirect('tweetbot:///mute/keyword?muteLists=1&regex=0&text=' + filterPattern);
+        var filterPattern = filterModel.attributes.pattern;
+        var filterType    = filterModel.attributes.type;
+        if (filterType === 'regex') {
+          res.redirect('tweetbot:///mute/keyword?muteLists=1&regex=1&text=' + filterPattern);
+        } else {
+          res.redirect('tweetbot:///mute/keyword?muteLists=1&regex=0&text=' + filterPattern);
+        }
       }
-    });
+    } else {
+      var error = new Error();
+      error.status = 404;
+      error.message = 'Looks like you\'ve either navigated to a page that doesn\'t exist or you followed a broken link to get here';   
+      next(error);
+    }
+  });
 });
 
 module.exports = router;
