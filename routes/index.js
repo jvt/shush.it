92ef1a73684111481234338e4a152d4aaee94c9c
Joe Torraca <joe@joetorraca.com>
2015-07-20 01:37:56 -0400

    Added filter and filter_export_log models in preparation for dynamic homepage

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var filter   = require('../models/filter').model;
+var exFilter = require('../models/exportedFilter').model;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
