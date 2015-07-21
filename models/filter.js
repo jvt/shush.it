547ae16751a9b6107563fb60040906d105b9a703
Joe Torraca <joe@joetorraca.com>
2015-07-20 23:50:57 -0400

    Updated vaidations to allow for spaces in title/description

--- models/filter.js
+++ models/filter.js
@@ -1,5 +1,6 @@
 var bookshelf = require('bookshelf').DB;
 var checkit   = require('checkit');
+var xss = require('xss');
 
 var User = require('./user').model;
 var ExportedFilter = require('./exportedfilter').model;
@@ -7,6 +8,7 @@ var ExportedFilter = require('./exportedfilter').model;
 exports.model = bookshelf.Model.extend({
   constructor: function() {
     bookshelf.Model.apply(this, arguments); // super()
+    this.on('saving', this.sanitize.bind(this));
     this.on('saving', this.validate.bind(this));
   },
   tableName: 'filters',
@@ -22,13 +24,19 @@ exports.model = bookshelf.Model.extend({
 
   validations: {
     owner: ['required', 'numeric', 'natural'],
-    title: ['required', 'alphaDash', 'maxLength:75'],
-    description: ['required', 'alphaDash', 'maxLength:255'],
+    title: ['required', 'maxLength:75'],
+    description: ['required', 'maxLength:255'],
     type: ['required'],
     pattern: ['required', 'maxLength:255']
   },
   validate: function() {
     return new checkit(this.validations).run(this.toJSON());
+  },
+  sanitize: function() {
+    this.attributes.title = xss(this.attributes.title);
+    this.attributes.description = xss(this.attributes.description);
+    this.attributes.pattern = xss(this.attributes.pattern);
+    return true;
   }
 
  });
